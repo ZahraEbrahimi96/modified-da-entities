c1fda268eec25556e0e850dec52267c8ed57c275
ZahraEbrahimi96 <ebrahimi.zahra1996@gmail.com>
2024-05-26 20:09:04 +0330

    update

--- src/main/java/exportation/model/da/CompanyDa.java
+++ src/main/java/exportation/model/da/CompanyDa.java
@@ -1,11 +1,15 @@
 package exportation.model.da;
 
 import exportation.model.entity.Company;
-import lombok.extern.log4j.Log4j;
+import exportation.model.entity.enums.RoleAccess;
 import exportation.model.tools.CRUD;
 import exportation.model.tools.ConnectionProvider;
-
-import java.sql.*;
+import lombok.extern.log4j.Log4j;
+import exportation.model.entity.Person;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,99 +22,128 @@ public class CompanyDa implements AutoCloseable, CRUD<Company> {
         connection = ConnectionProvider.getConnectionProvider().getConnection();
     }
 
-    //save
     @Override
-    public Company save(Company company) throws Exception {
-        company.setId(ConnectionProvider.getConnectionProvider().getNextId("COMPANY_SEQ"));
-        preparedStatement = connection.prepareStatement(
-                "INSERT INTO COMPANY (ID,NAME,PRODUCT,ADDRESS,EMAIL,PHONE) VALUES (?,?,?,?,?,?)"
-        );
-        preparedStatement.setInt(1, company.getId());
-        preparedStatement.setString(2, company.getName());
-        preparedStatement.setString(3, company.getProduct());
-        preparedStatement.setString(4, company.getAddress());
-        preparedStatement.setString(5, company.getEmail());
-        preparedStatement.setString(6, company.getPhoneNumber());
-        preparedStatement.execute();
-        return company;
+    public Company save(Company company) throws SQLException {
+    company.setCompanyId(ConnectionProvider.getConnectionProvider().getNextId("COMPANY_SEQ"));
+
+    preparedStatement = connection.prepareStatement("INSERT INTO COMPANY (COMPANY_ID, COMPANY_NAME, COMPANY_TYPE, COMPANY_MANAGER, COMPANY_PRODUCT, COMPANY_ADDRESS, COMPANY_EMAIL, COMPANY_PHONE, COUNTRY_NAME) VALUES (?, ?,?,?,?,?,?,?,?)");
+    preparedStatement.setInt(1, company.getCompanyId());
+    preparedStatement.setString(2, company.getCompanyName());
+    preparedStatement.setString(3, String.valueOf(company.getCompanyType()));
+    preparedStatement.setString(4, String.valueOf(company.getCompanyManager()));
+    preparedStatement.setString(5, company.getCompanyProduct());
+    preparedStatement.setString(6, company.getCompanyAddress());
+    preparedStatement.setString(7, company.getCompanyEmail());
+    preparedStatement.setString(8, company.getCompanyPhone());
+    preparedStatement.setString(9,String.valueOf(company.getCountryName()));
+    preparedStatement.execute();
+    return company;
     }
 
-    //Edit
     @Override
     public Company edit(Company company) throws Exception {
-        preparedStatement = connection.prepareStatement(
-                "UPDATE COMPANY SET NAME=?, PRODUCT=?, ADDRESS=?,EMAIL=?,PHONENUMBER, WHERE ID=?"
-        );
-        preparedStatement.setInt(1, company.getId());
-        preparedStatement.setString(2, company.getName());
-        preparedStatement.setString(3, company.getProduct());
-        preparedStatement.setString(4, company.getAddress());
-        preparedStatement.setString(5, company.getEmail());
-        preparedStatement.setString(6, company.getPhoneNumber());
-        preparedStatement.execute();
-        return company;
+            preparedStatement = connection.prepareStatement("UPDATE COMPANY SET COMPANY_NAME=?, COMPANY_TYPE=?, COMPANY_MANAGER=?, COMPANY_PRODUCT=?, COMPANY_ADDRESS=?, COMPANY_EMAIL=?, COMPANY_PHONE=?, COUNTRY_NAME=? WHERE COMPANY_ID=? ");
+
+            preparedStatement.setString(1, company.getCompanyName());
+            preparedStatement.setString(2, String.valueOf(company.getCompanyType()));
+            preparedStatement.setString(3, String.valueOf(company.getCompanyManager()));
+            preparedStatement.setString(4, company.getCompanyProduct());
+            preparedStatement.setString(5, company.getCompanyAddress());
+            preparedStatement.setString(6, company.getCompanyEmail());
+            preparedStatement.setString(7, company.getCompanyPhone());
+            preparedStatement.setString(8,String.valueOf(company.getCountryName()));
+            preparedStatement.setInt(9, company.getCompanyId());
+            preparedStatement.execute();
+            return company;
     }
 
-    //Remove
     @Override
-    public Company remove(int id) throws Exception {
-        preparedStatement = connection.prepareStatement(
-                "DELETE FROM COMPANY WHERE ID=?"
-        );
-        preparedStatement.setInt(1, id);
+    public Company remove(int companyId) throws Exception {
+        preparedStatement = connection.prepareStatement("DELETE FROM COMPANY WHERE COMPANY_ID=?");
+        preparedStatement.setInt(1, companyId);
         preparedStatement.execute();
         return null;
     }
 
-    //FindALl
     @Override
     public List<Company> findAll() throws Exception {
         List<Company> companyList = new ArrayList<>();
-        preparedStatement = connection.prepareStatement("SELECT * FROM COMPANY ORDER BY ID");
+        preparedStatement = connection.prepareStatement("SELECT * FROM COMPANY ORDER BY COMPANY_ID");
         ResultSet resultSet = preparedStatement.executeQuery();
 
         while (resultSet.next()) {
+
             Company company = Company
                     .builder()
-                    .id(resultSet.getInt("ID"))
-                    .name(resultSet.getString("NAME"))
-                    .product(resultSet.getString("PRODUCT"))
-                    .address(resultSet.getString("ADDRESS"))
-                    .email(resultSet.getString("EMAIL"))
-                    .phoneNumber(resultSet.getString("PHONENUMBER"))
+                    .companyId(resultSet.getInt("COMPANY_ID"))
+                    .companyName(resultSet.getString("COMPANY_NAME"))
+                    .companyType(RoleAccess.valueOf(resultSet.getString("COMPANY_TYPE")))
+                    .companyManager(resultSet.getString("COMPANY_MANAGER")))
+                    .companyProduct(resultSet.getString("COMPANY_PRODUCT"))
+                    .companyAddress(resultSet.getString("COMPANY_ADDRESS"))
+                    .companyEmail(resultSet.getString("COMPANY_EMAIL"))
+                    .companyPhone(resultSet.getString("COMPANY_PHONE"))
+                    .countryName(resultSet.getString("COUNTRY_NAME"))
                     .build();
 
-            companyList.add(company);
+                    companyList.add(company);
         }
-
         return companyList;
     }
 
-    //FindById
     @Override
-    public Company findById(int id) throws Exception {
-        preparedStatement = connection.prepareStatement("SELECT * FROM COMPANY WHERE ID=?");
-        preparedStatement.setInt(1, id);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        Company company = null;
-        if (resultSet.next()) {
-            company = Company
-                    .builder()
-                    .id(resultSet.getInt("ID"))
-                    .name(resultSet.getString("NAME"))
-                    .product(resultSet.getString("PRODUCT"))
-                    .address(resultSet.getString("ADDRESS"))
-                    .email(resultSet.getString("EMAIL"))
-                    .phoneNumber(resultSet.getString("PHONENUMBER"))
-                    .build();
-        }
+    public Company findById(int companyId) throws Exception {
+       preparedStatement=connection.prepareStatement("SELECT * FROM COMPANY WHERE COMPANY_ID=?");
+       preparedStatement.setInt(1, companyId);
+       ResultSet resultSet = preparedStatement.executeQuery();
+       Company company = null;
+
+       if(resultSet.next()) {
+         company = Company
+                 .builder()
+                 .companyId(resultSet.getInt("COMPANY_ID"))
+                 .companyName(resultSet.getString("COMPANY_NAME"))
+                 .companyType(RoleAccess.valueOf(resultSet.getString("COMPANY_TYPE")))
+                 .companyManager(resultSet.getString("COMPANY_MANAGER"))
+                 .companyProduct(resultSet.getString("COMPANY_PRODUCT"))
+                 .companyAddress(resultSet.getString("COMPANY_ADDRESS"))
+                 .companyEmail(resultSet.getString("COMPANY_EMAIL"))
+                 .companyPhone(resultSet.getString("COMPANY_PHONE"))
+                 .countryName(resultSet.getString("COUNTRY_NAME"))
+                 .build();
+       }
         return company;
     }
 
-    //Close
+
+    public List<Company> findByName (String companyName) throws Exception{
+        List<Company> companyList = new ArrayList<>();
+
+        preparedStatement = connection.prepareStatement("SELECT * FROM COMPANY WHERE COMPANY_NAME LIKE? ORDER BY COMPANY_ID");
+        preparedStatement.setString(1, companyName + "%");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while(resultSet.next()){
+            Company company = Company
+                    .builder()
+                    .companyId(resultSet.getInt("COMPANY_ID"))
+                    .companyName(resultSet.getString("COMPANY_NAME"))
+                    .companyType(RoleAccess.valueOf(resultSet.getString("COMPANY_TYPE")))
+                    .companyManager(resultSet.getString("COMPANY_MANAGER"))
+                    .companyProduct(resultSet.getString("COMPANY_PRODUCT"))
+                    .companyAddress(resultSet.getString("COMPANY_ADDRESS"))
+                    .companyEmail(resultSet.getString("COMPANY_EMAIL"))
+                    .companyPhone(resultSet.getString("COMPANY_PHONE"))
+                    .countryName(resultSet.getString("COUNTRY_NAME"))
+                    .build();
+            companyList.add(company);
+        }
+        return companyList;
+    }
+
+
     @Override
     public void close() throws Exception {
-        preparedStatement.close();
-        connection.close();
+
     }
 }
