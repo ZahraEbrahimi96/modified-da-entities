d18e7162a760efbf2ec307ed63210941c3175db3
ali_he96 <alihesaraki75@gmail.com>
2024-05-26 20:36:04 +0330

    create trade_table in db.sql

--- src/main/java/exportation/model/da/db.sql
+++ src/main/java/exportation/model/da/db.sql
@@ -1,6 +1,6 @@
 create table person_table
 (
-    person_id   number primary key,
+    person_id          number primary key,
     person_name        nvarchar2(30),
     person_family      nvarchar2(30),
     person_gender      varchar2(6),
@@ -14,7 +14,7 @@ create table person_table
 
 create table company_table
 (
-    company_id   number primary key,
+    company_id           number primary key,
     company_name         nvarchar2(30),
     company_type         nvarchar2(30),
     company_product      nvarchar2(30),
@@ -25,15 +25,26 @@ create table company_table
 
 create table country_table
 (
-    country_id     number primary key,
+    country_id             number primary key,
     country_name           nvarchar2(30),
     country_phone_code     nvarchar2(4),
     country_related_market nvarchar2(30)
 );
 
+create table trade_table
+(
+    trade_id              number primary key,
+    trade_client          nvarchar2(30),
+    trade_status          nvarchar2(30),
+    trade_correspondences nvarchar2(30),
+    trade_contract        nvarchar2(30),
+    trade_agreement       nvarchar2(30),
+    trade_invoice         nvarchar2(30)
+);
+
 create table itemTable
 (
-    item_id           number primary key,
+    item_id                number primary key,
     item_name              nvarchar2(30),
     item_model             nvarchar2(30),
     item_dimensionOfUnite  nvarchar2(30),
@@ -54,7 +65,7 @@ create table country_company
 create view country_company_report as
 select *
 from country_table
-         join country_company on country_table.id = country_company.c_country_id
-         join company_table on company_table.id = country_company.c_company_id
+         join country_company on country_table.country_id = country_company.c_country_id
+         join company_table on company_table.company_id = country_company.c_company_id
 
 -- todo : create sequences
