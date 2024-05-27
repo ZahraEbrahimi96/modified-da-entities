260d2e0500dd44daafe0f18cecf0ee0f572cf5e2
ali_he96 <alihesaraki75@gmail.com>
2024-05-28 00:30:09 +0330

    create PaymentBl

---
+++ src/main/java/exportation/controller/exceptions/NoPaymentFoundException.java
@@ -0,0 +1,7 @@
+package exportation.controller.exceptions;
+
+public class NoPaymentFoundException extends Exception {
+    public NoPaymentFoundException() {
+        super("No Payment Found !!!");
+    }
+}
