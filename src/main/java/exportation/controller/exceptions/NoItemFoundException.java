2bdf46517392d9471abf9e33d6b69ed5f12c5225
ali_he96 <alihesaraki75@gmail.com>
2024-05-28 00:26:25 +0330

    create ItemBl

---
+++ src/main/java/exportation/controller/exceptions/NoItemFoundException.java
@@ -0,0 +1,7 @@
+package exportation.controller.exceptions;
+
+public class NoItemFoundException extends Exception {
+    public NoItemFoundException() {
+        super("No Item Found !!!");
+    }
+}
