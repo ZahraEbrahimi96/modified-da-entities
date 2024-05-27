7c9605906a4824515b9e1aa50b0c30fb7137c653
ali_he96 <alihesaraki75@gmail.com>
2024-05-28 00:24:11 +0330

    create InfoBl

---
+++ src/main/java/exportation/controller/exceptions/NoInfoFoundException.java
@@ -0,0 +1,7 @@
+package exportation.controller.exceptions;
+
+public class NoInfoFoundException extends Exception {
+    public NoInfoFoundException() {
+        super("No Info Found !!!");
+    }
+}
