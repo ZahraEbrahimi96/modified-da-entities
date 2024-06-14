84bf7ca9e3d0fc697c7c27e2162f9ae499791983
ali_he96 <alihesaraki75@gmail.com>
2024-06-14 13:45:22 +0330

    update Da

--- src/main/java/exportation/view/WindowsManager.java
+++ src/main/java/exportation/view/WindowsManager.java
@@ -5,13 +5,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class WindowsManager {
 
     public static void showPersonForm() throws IOException {
         Stage stage = new Stage();
         Scene scene = new Scene(
-                FXMLLoader.load(WindowsManager.class.getResource("view/profile.fxml"))
+                FXMLLoader.load(Objects.requireNonNull(WindowsManager.class.getResource("view/profile.fxml")))
         );
 
         stage.setScene(scene);
@@ -22,7 +23,7 @@ public class WindowsManager {
     public static void showStuffForm() throws IOException {
         Stage stage = new Stage();
         Scene scene = new Scene(
-                FXMLLoader.load(WindowsManager.class.getResource("view/stuff.fxml"))
+                FXMLLoader.load(Objects.requireNonNull(WindowsManager.class.getResource("view/stuff.fxml")))
         );
 
         stage.setScene(scene);
@@ -33,7 +34,7 @@ public class WindowsManager {
     public static void showAboutForm() throws IOException {
         Stage stage = new Stage();
         Scene scene = new Scene(
-                FXMLLoader.load(WindowsManager.class.getResource("about.fxml"))
+                FXMLLoader.load(Objects.requireNonNull(WindowsManager.class.getResource("about.fxml")))
         );
 
         stage.setScene(scene);
